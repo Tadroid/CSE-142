127678f84a8fcf311d2cbc1fa451a727e0dd8be9
Joseph Herkness <jpherkness@gmail.com>
2014-12-08 12:06:55 -0500

    Update 7_12.c

--- Chapter-7/7_12.c
+++ Chapter-7/7_12.c
@@ -1,13 +1,7 @@
 /*
  Author:    Joseph Herkness
- Date:      2014-12-1
- Objective: Modify the program in Fig. 7.24 so that the card-dealing function deals a five-card poker hand. Then write the following additional functions:
-            a) Determine whether the hand contains a pair.
-            b) Determine whether the hand contains two pairs.
-            c) Determine whether the hand contains three of a kind (e.g., three jacks).
-            d) Determine whether the hand contains four of a kind (e.g., four aces).
-            e) Determine whether the hand contains a flush (i.e., all five cards of the same suit).
-            f) Determine whether the hand contains a straight (i.e., five cards of consecutive face values).
+ Date:      2014-12-6
+ Objective: Use the functions developed in Exercise 7.12 to write a program that deals two five-card poker hands, evaluates each, and determines which is the better hand.
  */
 
 #include <stdio.h>
@@ -17,37 +11,40 @@
 #define SUITS 4
 #define FACES 13
 #define CARDS 52
-#define PLAYERS 3
 
 // prototypes
 void shuffle( unsigned int wDeck[][ FACES ] );
-void deal(int hands[][5], unsigned int wDeck[][ FACES ], const char *wFace[], const char *wSuit[] );
+void deal(int hand[], unsigned int wDeck[][ FACES ], const char *wFace[], const char *wSuit[] );
 int onePair(int hand[], const char *wFace[]);
 int twoPair(int hand[], const char *wFace[]);
 int threeOfAKind(int hand[], const char *wFace[]);
 int fourOfAKind(int hand[], const char *wFace[]);
 int flush(int hand[], const char *wSuit[]);
-int straight(int hand[], const char *wface[]);
+int straight(int hand[], const char *wFace[]);
 
 int main( void ){
     const char *suit[ SUITS ] = { "Hearts", "Diamonds", "Clubs", "Spades" }; // initialize suit array
     
     const char *face[ FACES ] = { "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };// initialize face array
-
+    
     unsigned int deck[ SUITS ][ FACES ] = {0}; // initialize deck array
     
     srand((unsigned int)time(NULL)); // seed random-number generator
     
     shuffle(deck); // shuffle the deck
     
-    int hands[PLAYERS][5]; // initialize and array of hands
+    int hand[5]; // initialize and array of hands
     
-    deal(hands, deck, face, suit); // deal hands
+    deal(hand, deck, face, suit); // deal hand
     
     return 0;
 }
 
-// shuffle cards in deck
+/**
+ *  Takes a deck and filles it with randomized cards
+ *
+ *  @param wDeck array (with each element initialized to 0)
+ */
 void shuffle( unsigned int wDeck[][FACES]){
     size_t row; // row number
     size_t column; // column number
@@ -67,45 +64,49 @@ void shuffle( unsigned int wDeck[][FACES]){
     }
 }
 
-// deal cards in deck
-void deal(int hands[][5], unsigned int wDeck[][FACES], const char *wFace[],const char *wSuit[]){
+/**
+ *  Takes a array array of cards and distributes them among each player
+ *
+ *  @param hands array of hands to be filled
+ *  @param wDeck array of cards
+ *  @param wFace character array representing face names
+ *  @param wSuit character array representing suit names
+ */
+void deal(int hand[5], unsigned int wDeck[][FACES], const char *wFace[],const char *wSuit[]){
     size_t card; // card counter
     size_t row; // row counter
     size_t column; // column counter
     
-    for(int player = 0; player < PLAYERS; player++){
-        printf("Player %d: \n\n", player + 1);
-        // deal each of the cards
-        for ( card = 0; card < 5; card++ ) {
-            // loop through rows of wDeck
-            for ( row = 0; row < SUITS; ++row ) {
-                // loop through columns of wDeck for current row
-                for ( column = 0; column < FACES; column++ ) {
-                    // if slot contains current card, display card
-                    if ( wDeck[ row ][ column ] == card + 1 + (player * 5)) {
-                        printf( "%5s of %-8s\n", wFace[column], wSuit[row]);
-                        hands[player][card] = (int)((column) + (row * FACES));
-                    }
+    // deal each of the cards
+    for ( card = 0; card < 5; card++ ) {
+        // loop through rows of wDeck
+        for ( row = 0; row < SUITS; ++row ) {
+            // loop through columns of wDeck for current row
+            for ( column = 0; column < FACES; column++ ) {
+                // if slot contains current card, display card
+                if ( wDeck[ row ][ column ] == card + 1) {
+                    printf( "%5s of %-8s\n", wFace[column], wSuit[row]);
+                    hand[card] = (int)((column) + (row * FACES));
                 }
             }
         }
+    }
         
-        printf("\n"); //Spacer
-        if(straight(hands[player], wFace) == 1){
-            printf("[You have a striaght]\n");
-        }else if(flush(hands[player], wSuit) == 1){
-            printf("[You have flush]\n");
-        }else if(fourOfAKind(hands[player], wFace) == 1){
-            printf("[You have four of a kind]\n");
-        }else if(threeOfAKind(hands[player], wFace) == 1){
-            printf("[You have three of a kind]\n");
-        }else if(twoPair(hands[player], wFace) == 1){
-            printf("[You have two pairs]\n");
-        }else if(onePair(hands[player], wFace) == 1){
-            printf("[You have a pair]\n");
-        }else{
-            printf("[You have nothing]\n");
-        }
+    printf("\n"); //Spacer
+    if(straight(hand, wFace) == 1){
+        printf("> [You have a striaght]\n\n");
+    }else if(flush(hand, wSuit) == 1){
+        printf("> [You have flush]\n\n");
+    }else if(fourOfAKind(hand, wFace) == 1){
+        printf("> [You have four of a kind]\n\n");
+    }else if(threeOfAKind(hand, wFace) == 1){
+        printf("> [You have three of a kind]\n\n");
+    }else if(twoPair(hand, wFace) == 1){
+        printf("> [You have two pairs]\n\n");
+    }else if(onePair(hand, wFace) == 1){
+        printf("> [You have a pair]\n\n");
+    }else{
+        printf("> [You have nothing]\n\n");
     }
 }
 
